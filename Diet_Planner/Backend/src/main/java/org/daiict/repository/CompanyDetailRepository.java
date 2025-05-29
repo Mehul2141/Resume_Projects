2a3beebd1f247d264d813f3961426d70102f68da
Mehul Bhuva <202001437@daiict.ac.in>
2025-05-29 23:50:00 +0530

    Cleaned Diet Planner project

--- Diet_Planner/Backend/src/main/java/org/daiict/repository/CompanyDetailRepository.java
+++
@@ -1,9 +0,0 @@
-package org.daiict.repository;
-
-import org.daiict.model.CompanyDetail;
-import org.daiict.model.UserPortfolio;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CompanyDetailRepository extends JpaRepository<CompanyDetail, String> {
-
-}
--- Diet_Planner/Backend/src/main/resources/mysql-setup.sql
+++
@@ -1,3 +0,0 @@
-CREATE DATABASE user_details;
-CREATE USER 'newuser'@'localhost' IDENTIFIED BY 'Dhruv@#123';
-GRANT ALL ON user_details.* TO 'newuser'@'localhost';
